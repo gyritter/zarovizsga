aa5210c9cd7c6474cefbf0b09740e5b6f19cec4a
gyuri <gyritter@digikabel.hu>
2021-03-16 21:05:16 +0100

    zarovizsga 5

--- src/main/java/hu/nive/ujratervezes/zarovizsga/dogtypes/DogTypes.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/dogtypes/DogTypes.java
@@ -1,7 +1,10 @@
 package hu.nive.ujratervezes.zarovizsga.dogtypes;
 
 import javax.sql.DataSource;
+import java.text.Collator;
+import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 public class DogTypes {
 
@@ -13,6 +16,8 @@ public class DogTypes {
 
     public List<String> getDogsByCountry(String country){
         DogTypesDao dogTypesDao=new DogTypesDao();
-        return dogTypesDao.selectDogsByCountry(dataSource,country);
+        List<String> result = dogTypesDao.selectDogsByCountry(dataSource,country);
+        Collections.sort(result, Collator.getInstance(new Locale("hu", "HU")));
+        return result;
     }
 }
